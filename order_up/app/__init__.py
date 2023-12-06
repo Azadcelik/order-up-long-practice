830abfe725676f66bce4b4d7f0eade34496206b7
Yun Zhang <zhangyun024@gmail.com>
2023-12-06 14:44:20 -0800

    Making the login view done

--- order_up/app/__init__.py
+++ order_up/app/__init__.py
@@ -1,7 +1,24 @@
 from flask import Flask
 from .config import Configuration
+from .models import db   # New import
 from .routes import orders
+from .routes import session
+from flask_login import LoginManager
+from .models import db, Employee
+
+
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 app.register_blueprint(orders.bp)
+app.register_blueprint(session.bp)
+
+db.init_app(app)
+
+login = LoginManager(app)
+login.login_view = "session.login"
+
+
+@login.user_loader
+def load_user(id):
+    return Employee.query.get(int(id))
